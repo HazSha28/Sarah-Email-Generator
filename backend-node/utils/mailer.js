0aeea6cb618abe61758e4db0da4df6c9dfaf500d
Hasee28 <tohazsha@gmail.com>
2026-06-29 16:26:21 +0530

    Fix mailer to create transporter fresh per send, add logging

--- backend-node/utils/mailer.js
+++ backend-node/utils/mailer.js
@@ -1,20 +1,30 @@
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
-  }
-});
-
 const sendEmail = async ({ to, subject, html }) => {
+  // Create transporter fresh each time to pick up env vars correctly
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  console.log(`Sending email to ${to} from ${process.env.MAIL_USER}`);
+
   await transporter.sendMail({
     from: `"Sarah Jewellers" <${process.env.MAIL_USER}>`,
     to,
     subject,
     html
   });
+
+  console.log(`Email successfully sent to ${to}`);
 };
 
 module.exports = { sendEmail };
